93621ba3c43fd3603b6fe491b0d1a454a54587ea
salamon240 <82862350+salamon240@users.noreply.github.com>
2021-07-18 15:36:37 +0300

    Delete 121.sql

--- 121.sql
+++
@@ -1,1 +0,0 @@
-﻿select b.phonNumber,b.Address,b.name,MeetingDate,m.HairCut,t.Time from Users u join Meetings m on u.UserID=m.UserID join Barbushops b on m.MUserID=b.UserID join Times t on t.MeetingTime=m.MeetingTime where u.UserID=9
